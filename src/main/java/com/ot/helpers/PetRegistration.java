01a831b2f5fd9a3c2264dada63e91f1cea997fa6
Olga Turaeva <olga@olgas-mbp.gateway.sonic.net>
2016-08-17 20:02:56 -0700

    Create PetRegistration

---
+++ src/main/java/com/ot/helpers/PetRegistration.java
@@ -0,0 +1,38 @@
+/**
+ * File Name: PetRegistration.java<br>
+ * Turaeva, Olga<br>
+ * Java Boot Camp Exercise<br>
+ * Instructor: Jean-francois Nepton<br>
+ * Created: Aug 17, 2016
+ */
+/*
+ * Welcome the user Ask for number of pets to register Initial arrays with
+ * defined size Iterate through the defined amount and ask for the pet details
+ * while populating the arrays Display all animal details by iterating after the
+ * details were stored Farewell the user
+ */
+package com.ot.helpers;
+
+/**
+ * PetRegistration //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ *
+ * @author Turaeva, Olga
+ * @version 1.0.0
+ * @since 1.0
+ */
+public class PetRegistration {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Pet Registration page");
+		int[] petNumbers = { 3, 6, 8 };
+		String[] petNames = { "Liza", "Bobby", "Chuck" };
+	}
+}
